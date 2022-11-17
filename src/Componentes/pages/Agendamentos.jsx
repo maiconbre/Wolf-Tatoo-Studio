ffd5d2a720aa5126ba6ac2799d34a3efff87765f
Maicon Brendon <112555118+maiconbre@users.noreply.github.com>
2022-11-16 22:17:03 -0300

    ok

--- src/Componentes/pages/Agendamentos.jsx
+++ src/Componentes/pages/Agendamentos.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -11,7 +12,7 @@ export const Agendamentos = () => {
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="/agendamento">Agendamento</Nav.Link>
+              <Nav.Link href="/agendamentos">Agendamentos</Nav.Link>
               <Nav.Link href="/studios">Studios</Nav.Link>
               <Nav.Link href="/equipe">Equipe</Nav.Link>
             </Nav>
