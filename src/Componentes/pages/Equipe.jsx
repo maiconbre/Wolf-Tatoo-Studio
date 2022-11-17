71e6c296a1ef04dfd7c0af0ec7538f2210550aac
Maicon Brendon <112555118+maiconbre@users.noreply.github.com>
2022-11-16 21:55:53 -0300

    edit

--- src/Componentes/pages/Equipe.jsx
+++ src/Componentes/pages/Equipe.jsx
@@ -4,11 +4,11 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-export const Home = () =>{
+export const Equipe = () =>{
     return (
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
-        <Navbar.Brand href="#home">Wolf Tatoo Studio</Navbar.Brand>
+        <Navbar.Brand href="/">Wolf Tatoo Studio</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
