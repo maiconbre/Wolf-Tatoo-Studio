89779185a2061537a8770e9db36884509d8caacd
Tamires <112409145+Tamiresporto@users.noreply.github.com>
2022-11-16 23:04:55 -0300

    Update Studios.jsx

--- src/Componentes/pages/Studios.jsx
+++ src/Componentes/pages/Studios.jsx
@@ -48,6 +48,18 @@ export const Studios = () =>{
         <Button variant="primary">Go somewhere</Button>
       </Card.Body>
     </Card>
+
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="/src/Componentes//pages/tatuador1.jpg" width="300" height="200" />
+      <Card.Body>
+        <Card.Title>Leonardo</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
     </div>
     
     
