91a856e236ad59922a37f4a3f1864ba5fb3408b7
Maicon Brendon <112555118+maiconbre@users.noreply.github.com>
2022-11-19 19:11:53 -0300

    1

--- src/Componentes/layout/Navbar/Navbara.jsx
+++ src/Componentes/layout/Navbar/Navbara.jsx
@@ -2,12 +2,13 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './navbar.css';
 
 function Navbara () {
     return (
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
-        <Navbar.Brand href="/">Wolf Tattoo Studio</Navbar.Brand>
+        <Navbar.Brand  href="/">Wolf Tattoo Studio</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
