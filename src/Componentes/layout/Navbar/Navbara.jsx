a055736cd0d489b1dfc4a063f509d668fc05ddbe
Maicon Brendon <112555118+maiconbre@users.noreply.github.com>
2022-11-20 02:44:07 -0300

    1

--- src/Componentes/layout/Navbar/Navbara.jsx
+++ src/Componentes/layout/Navbar/Navbara.jsx
@@ -6,6 +6,7 @@ import './navbar.css';
 
 function Navbara () {
     return (
+<<<<<<< Updated upstream
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
         <Navbar.Brand  href="/">Wolf Tattoo Studio</Navbar.Brand>
@@ -19,6 +20,35 @@ function Navbara () {
         </Navbar.Collapse>
       </Container>
     </Navbar>
+=======
+      <div>
+      <header>
+          <div className='w3-top' id='home'>
+              <div className='w3-bar  w3-card w3-left-align w3-large heeder-large-screen' >
+                  <a className='w3-bar-item w3-button w3-hide-medium w3-hide-large w3-right w3-padding-large w3-hover-black w3-large ' title='Toggle Navigation Menu'><i className='fa fa-bars'></i></a>
+                  <a href='#home' className='w3-bar-item w3-button w3-padding-large  w3-hover-black'>Wolf Studio Tattoo</a>
+                  <div >
+                      <a href='/' className='w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-black' >Home</a>
+                      <a href='/studios' className='w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-black'> Studios</a>
+                      <a href='/agendamentos' className='w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-black'>Agendamento</a>
+                      <a href='/equipe' className='w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-black'>Sobre</a>
+                      
+                  </div>
+
+              </div>
+
+              {/* Navbar on small screens */}
+              <div id='navDemo' className='w3-bar-block  w3-hide w3-hide-large w3-hide-medium w3-large'>
+                  <a href='/' className='w3-bar-item w3-button w3-padding-large' >Home</a>
+                  <a href='/studios' className='w3-bar-item w3-button w3-padding-large' >Studios</a>
+                  <a href='/agendamentos' className='w3-bar-item w3-button w3-padding-large' >Agendamento</a>
+                  <a href='/equipe' className='w3-bar-item w3-button w3-padding-large' >Sobre</a>
+                  
+              </div>
+          </div>
+      </header>
+  </div>
+>>>>>>> Stashed changes
     );
 }
 export default Navbara
--- src/Componentes/pages/Equipe/Equipe.jsx
+++ src/Componentes/pages/Equipe/Equipe.jsx
@@ -2,78 +2,79 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbara from '../../layout/Navbar/Navbara'
 import Footer from '../../layout/Footer/Footer';
-
-import './equipe.css';
-
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import '../../Style/Site.css'
+import './equipe.css'
 
 export const Equipe = () => {
   return (
     <div>
       <Navbara />
-      
-      <div class="card1">
-        <Card style={{ width: '18rem' }}>
-          <Card.Img variant="top" src="/src/Componentes/pages/img/kezia.jfif" width="300" height="200" />
-          <Card.Body>
-            <Card.Title>Kezia</Card.Title>
-            <Card.Text>
-              <i>Gestora de Enganjamento</i><br></br>
+      <div className='cards'>
+        <div class="card1">
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="/src/Componentes/pages/img/kezia.jfif" width="300" height="200" />
+            <Card.Body>
+              <Card.Title>Kezia</Card.Title>
+              <Card.Text>
+                <i>Gestora de Enganjamento</i><br></br>
 
-            </Card.Text>
-            <div class="buttom1"><Button variant="primary">Contato</Button></div>
-          </Card.Body>
-        </Card>
+              </Card.Text>
+              <div class="buttom1"><Button variant="primary">Contato</Button></div>
+            </Card.Body>
+          </Card>
+        </div>
+        <div class="card2">
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="/src/Componentes/pages/img/maicon.jfif" width="300" height="200" />
+            <Card.Body>
+              <Card.Title>Maicon</Card.Title>
+              <Card.Text>
+                <i> Co-Facilitador</i><br></br>
+              </Card.Text>
+              <Button variant="primary">Contato</Button>
+            </Card.Body>
+          </Card>
+        </div>
+        <div class="card3">
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="/src/Componentes/pages/img/tamires.jfif" width="300" height="200" />
+            <Card.Body>
+              <Card.Title>Tamires</Card.Title>
+              <Card.Text>
+                <i>Colaboradora l</i> <br></br>
+              </Card.Text>
+              <Button variant="primary">Contato</Button>
+            </Card.Body>
+          </Card></div>
+        <div class="card4">
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="/src/Componentes/pages/img/bernardo.jfif" width="300" height="200" />
+            <Card.Body>
+              <Card.Title><i>Bernardo</i></Card.Title>
+              <Card.Text>
+                <i>Gestora de Conhecimento</i><br></br>
+              </Card.Text>
+              <Button variant="primary">Contato</Button>
+            </Card.Body>
+          </Card></div>
+        <div class="card5">
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="/src/Componentes/pages/img/grazi.jpg" width="300" height="200" />
+            <Card.Body>
+              <Card.Title><i>Grazi</i></Card.Title>
+              <Card.Text>
+                Colaboradora ll<br></br>
+              </Card.Text>
+              <Button variant="primary">Contato</Button>
+            </Card.Body>
+          </Card>
+        </div>
       </div>
-      <div class="card2">
-        <Card style={{ width: '18rem' }}>
-          <Card.Img variant="top" src="/src/Componentes/pages/img/maicon.jfif" width="300" height="200" />
-          <Card.Body>
-            <Card.Title>Maicon</Card.Title>
-            <Card.Text>
-              <i> Co-Facilitador</i><br></br>
-            </Card.Text>
-            <Button variant="primary">Contato</Button>
-          </Card.Body>
-        </Card>
-      </div>
-      <div class="card3">
-        <Card style={{ width: '18rem' }}>
-          <Card.Img variant="top" src="/src/Componentes/pages/img/tamires.jfif" width="300" height="200" />
-          <Card.Body>
-            <Card.Title>Tamires</Card.Title>
-            <Card.Text>
-              <i>Colaboradora l</i> <br></br>
-            </Card.Text>
-            <Button variant="primary">Contato</Button>
-          </Card.Body>
-        </Card></div>
-      <div class="card4">
-        <Card style={{ width: '18rem' }}>
-          <Card.Img variant="top" src="/src/Componentes/pages/img/bernardo.jfif" width="300" height="200" />
-          <Card.Body>
-            <Card.Title><i>Bernardo</i></Card.Title>
-            <Card.Text>
-              <i>Gestora de Conhecimento</i><br></br>
-            </Card.Text>
-            <Button variant="primary">Contato</Button>
-          </Card.Body>
-        </Card></div>
-      <div class="card5">
-        <Card style={{ width: '18rem' }}>
-          <Card.Img variant="top" src="/src/Componentes/pages/img/grazi.jpg" width="300" height="200" />
-          <Card.Body>
-            <Card.Title><i>Grazi</i></Card.Title>
-            <Card.Text>
-              Colaboradora ll<br></br>
-            </Card.Text>
-            <Button variant="primary">Contato</Button>
-          </Card.Body>
-        </Card></div>
-      
-      
-      
+
+
+
       <Footer />
     </div>
 
