824d9b7e8ea4a2b68b2602cbc5f85759b74b2aa9
Maicon Brendon <112555118+maiconbre@users.noreply.github.com>
2022-11-21 15:44:36 -0300

    ok

--- src/Componentes/layout/Tatuador1/Tatuador1.jsx
+++ src/Componentes/layout/Tatuador1/Tatuador1.jsx
@@ -4,60 +4,59 @@ import '../../Style/Site.css';
 
 function Tatuador1() {
     return (
-        <div className='tatuador'>
-            <div className='store' id='shop'>
+
+        <div>
+
+            <div className='tatuador'>
                 <br />
-                <h2 className='title'>Joao Marcos</h2>
+                <h2 className='title'>Joao </h2>
+                <div className='store' id='shop'>
+                    <div className='game-store'>
+                        <div className='game-store-list'>
+                            <div className='game-image'>
+                                <img src='https://raw.githubusercontent.com/maiconbre/Wolf-Tatoo-Studio/main/src/Componentes/pages/img/tatuador2.jpeg' />
+                            </div>
+                        </div>
+                    </div>
+                    <div className='bio'>
+                        <h4>João é carioca, formado em desing. Desde criança era apaixonado por tatuagem, principalmente pelas várias possibilidades de pigmentação e cores.
+                            <br />Pela falta de referências decidiu se especializar em trabalhos em pele negra trazendo um ambiente mais inclusivo, e elevando a autoestima das pessoas
+                            que encontram na Wolf Tatto Studio um ambiente acolhedor para todos.
+                            <br />É especialista há 15 anos em realismo, blackWork e old school. Ele iniciou sua carreira artística em 2001, quando cursou desenho artístico e aerografia. Esse foi o start para que em alguns anos mais tarde entrase para o universo das tatuagens </h4>
+                    </div>
+                </div>
+            </div>
+            <h2 className='title' id='artes'>Portifólio</h2>
+            <div className='games'>
+                <div className='game-list'>
+                    <img src='https://www.guiadasemana.com.br/contentFiles/image/2018/08/FEA/galeria/37265_w840h525_1533590127old-school.jpg' alt='old school' />
+                    <h3>Old School</h3>
+                </div>
+
+                <div className='game-list'>
+                    <img src='https://www.guiadasemana.com.br/contentFiles/image/2018/08/FEA/galeria/37265_w840h525_1533591561geometrica.jpg' alt='Geométrico' />
+                    <h3>Geométrico</h3>
+                </div>
+
+                <div className='game-list'>
+                    <img src='https://www.guiadasemana.com.br/contentFiles/image/2018/08/FEA/galeria/37265_w840h525_1533592552realista.jpg' alt='Realista' />
+                    <h3>Realista</h3>
+                </div>
+                <div className='game-list'>
+                    <img src='https://www.guiadasemana.com.br/contentFiles/image/2018/08/FEA/galeria/37265_w840h525_1533590977aquarela.jpg' alt='Aquarela' />
+                    <h3>Aquarela</h3>
+                </div>
+
+                <br />
+
+            </div>
+            <div className='tatuador'>
+                <br />
+                <h2 className='title'>Bruno</h2>
+                <div className='store' id='shop'></div>
                 <div className='game-store'>
                     <div className='game-store-list'>
                         <div className='game-image'>
-                            <img src='https://raw.githubusercontent.com/maiconbre/Wolf-Tatoo-Studio/main/src/Componentes/pages/img/tatuador2.jpeg' />
-                        </div>
-                        <div className='store-text-description'>
-                            <h4>Campo Grande</h4>
-                            nv: 15 anos
-                            <small>Pontilhismo Realismo Black Work Old School</small>
-                        </div>
-                    </div>
-                </div>
-                <div className='bio'>
-                    <h1>ETERNIZE SUAS IDEIAS</h1>
-                    <h4>João é carioca, formado em desing. Desde criança era apaixonado por tatuagem, principalmente pelas várias possibilidades de pigmentação e cores. 
-                        Pela falta de referências decidiu se especializar em trabalhos em pele negra trazendo um ambiente mais inclusivo, e elevando a autoestima das pessoas 
-                        que encontram na Wolf Tattoo Studio um ambiente acolhedor para todos. 
-                        É especialista há 15 anos em realismo, blackWork e old school. Ele iniciou sua carreira artística em 2001, quando cursou desenho artístico e aerografia. Esse foi o start para que em alguns anos mais tarde entrase para o universo das tatuagens </h4>
-                </div>
-            </div>
-            
-            <main>
-                <h2 className='title' id='artes'>Portifólio</h2>
-                <div className='games'>
-                    <div className='game-list'>
-                        <img src='https://www.guiadasemana.com.br/contentFiles/image/2018/08/FEA/galeria/37265_w840h525_1533590127old-school.jpg' alt='old school' />
-                        <h3>Old School</h3>
-                    </div>
-                   
-                    <div className='game-list'>
-                        <img src='https://www.guiadasemana.com.br/contentFiles/image/2018/08/FEA/galeria/37265_w840h525_1533592552realista.jpg' alt='Realista' />
-                        <h3>Realista</h3>
-                    </div>
-                    
-                    <div className='game-list'>
-                        <img src='https://www.guiadasemana.com.br/contentFiles/image/2018/08/FEA/galeria/37265_w840h525_1533654886blackwork-grande.jpg' alt='BlackWork' />
-                        <h3>Black Work</h3>
-                    </div>
-                    <div className='game-list'>
-                        <img src='https://www.guiadasemana.com.br/contentFiles/image/2018/08/FEA/galeria/37265_w840h525_1533589655pontilhismo.jpg' alt='Pontilhismo' />
-                        <h3>Pontilhismo</h3>
-                    </div>
-                    
-                   
-
-                </div>
-                <h2 className='title'>Bruno</h2>
-                <div className='game-store'>
-                    <div className='game-store-list'>
-                <div className='game-image'>
                             <img src='https://img.freepik.com/fotos-gratis/retrato-de-homem-serio-e-barbudo-em-oculos-escuros-e-chapeu-de-cilindro_613910-1612.jpg?w=996&t=st=1669038734~exp=1669039334~hmac=0ee3165dd312ed4140992115896acaa52d15468f6f03fdc889841c0443d72fb9' />
                         </div>
                         <div className='store-text-description'>
@@ -65,88 +64,84 @@ function Tatuador1() {
                             nv: 10 anos
                             <small>Maori, Aquarela, Sem contorno</small>
                         </div>
-                        </div>
-                        </div>
-                        <div className='bio'>
-                    <h1>ETERNIZE SUAS IDEIAS</h1>
-                    <h4>Historiador por formação, Bruno descobriu na tatuagem uma forma de eternizar e  por em prática todo o seu amor por história. 
-                        Começou com alguns desenhos simples, rabiscos e logo descobriu que realmente levava jeito pra coisa! Especialista em tatuagens étnicas, domina os mais variados
+                    </div>
+
+                </div>
+                <div className='bio'>
+                    <h4>Historiador por formação, Bruno descobriu na tatuagem uma forma de eternizar e  por em prática todo o seu amor por história.
+                        <br />Começou com alguns desenhos simples, rabiscos e logo descobriu que realmente levava jeito pra coisa! Especialista em tatuagens étnicas, domina os mais variados
                         estilos como Maori, Oriental e tem um amplo conhecimento de várias culturas.
-                        "Muitas pessoas se tatuam buscando significados ou eternizar momentos para o futuro. A tatuagem está presente na história da humanidade
-                        como forma de expressão, e eu percebi que havia um nicho de pessoas que queria se conectar mais com sua ancestralidade". 
-                        Com ideias e princípios underground e alternativas, foi conquistando seu espaço com seu estilo próprio de tatuagens, sendo elas maori, aquarela, sem contorno.</h4>
+                        <br />"Muitas pessoas se tatuam buscando significados ou eternizar momentos para o futuro. A tatuagem está presente na história da humanidade
+                        como forma de expressão, e eu percebi que havia um nicho de pessoas que queria se conectar mais com sua ancestralidade".
+                        <br />Com ideias e princípios underground e alternativas, foi conquistando seu espaço com seu estilo próprio de tatuagens, sendo elas maori, aquarela, sem contorno.</h4>
                 </div>
-                <h2 className='title' id='artes'>Portifólio</h2>
-                <div className='games'>
-                    <div className='game-list'>
-                        <img src='https://www.guiadasemana.com.br/contentFiles/image/2018/08/FEA/galeria/37265_w840h525_1533590977aquarela.jpg' />
-                        <h3>aquarela</h3>
-                    </div>
-                    <div className='game-list'>
-                        <img src='https://www.guiadasemana.com.br/contentFiles/image/2018/08/FEA/galeria/37265_w840h525_1533653325costela7.jpg' />
-                        <h3>Sem contorno</h3>
-                    </div>
-                    <div className='game-list'>
-                        <img src='https://www.guiadasemana.com.br/contentFiles/image/2018/08/FEA/galeria/37265_w840h525_1533592955maori.jpg' />
-                        <h3>Maori</h3>
-                    </div>
+            </div>
+
+            <div className='games'>
+                <div className='game-list'>
+                    <img src='https://www.guiadasemana.com.br/contentFiles/image/2018/08/FEA/galeria/37265_w840h525_1533590977aquarela.jpg' />
+                    <h3>aquarela</h3>
+                </div>
+                <div className='game-list'>
+                    <img src='https://www.guiadasemana.com.br/contentFiles/image/2018/08/FEA/galeria/37265_w840h525_1533653325costela7.jpg' />
+                    <h3>Sem contorno</h3>
+                </div>
+
+                <div className='game-list'>
+                    <img src='https://www.guiadasemana.com.br/contentFiles/image/2018/08/FEA/galeria/37265_w840h525_1533592955maori.jpg' alt='Maori' />
+                    <h3>Maori</h3>
+                </div>
+            </div>
+            <br />
+            <h2 className='title'>Gabi</h2>
+            <div className='game-store'>
+                <div className='game-store-list'>
+                    <div className='game-image'>
+                        <img src='https://img.freepik.com/fotos-gratis/jovem-mestre-de-tatuagem-focado-esta-fazendo-tatuagem-na-mao-do-cliente-no-salao-de-tatuagem-escuro_613910-5867.jpg?w=996&t=st=1669038700~exp=1669039300~hmac=1fe3678b6e2f80db5a477a5c8646ad660e724a7c6d4935d0a370d380e1711249' />
                     </div>
 
-                <h2 className='title'>Gabi</h2>
-                <div className='game-store'>
-                    <div className='game-store-list'>
-                <div className='game-image'>
-                            <img src='https://img.freepik.com/fotos-gratis/jovem-mestre-de-tatuagem-focado-esta-fazendo-tatuagem-na-mao-do-cliente-no-salao-de-tatuagem-escuro_613910-5867.jpg?w=996&t=st=1669038700~exp=1669039300~hmac=1fe3678b6e2f80db5a477a5c8646ad660e724a7c6d4935d0a370d380e1711249' />
-                        </div>
-                        <div className='store-text-description'>
-                            <h4>Campo Grande</h4>
-                            nv: 5 anos
-                            <small>Geometrico, oriental, single line, glitch, tinta vermelha</small>
-                        </div>
-                        </div>
-                        </div>
-                        <div className='bio'>
-                    <h1>ETERNIZE SUAS IDEIAS</h1>
-                    <h4>Gabi é formada em estética, e sua vontade de ajudar as pessoas a recuperar a autoestima foi o que a levou a fundar a Wolf Tattoo Studios.
-                        Especialista em em tatuar cicatrizes, Gabi tem a missão de ressignificar marcas que são dolorosas as pessoas e transformá-las de forma positiva. 
-                        Esse tipo de de trabalho exige um grande dominío das técnicas, além de talento já que cicatrizes apresentam um outro tipo de pele, que recebe a tintura de forma diferente. 
-                        A Woolf Tatto Studios surgiu  para trazer um sorriso ao rosto das pessoas, independente do motivo para fazer uma arte. 
-                        Seus principal estilo é o geométrico.</h4>
                 </div>
-                <h2 className='title' id='artes'>Portifólio</h2>
-                <div className='games'>
-                    <div className='game-list'>
-                        <img src='https://www.guiadasemana.com.br/contentFiles/image/2018/08/FEA/galeria/37265_w840h525_1533591561geometrica.jpg' />
-                        <h3>Geometrico</h3>
-                    </div>
-                    <div className='game-list'>
-                        <img src='https://fotostatuagens.com/wp-content/uploads/2018/04/tattoo-geometrica.jpg' />
-                        <h3>Geometrico</h3>
-                    </div>
-                    <div className='game-list'>
-                        <img src='https://tudocommoda.com/wp-content/uploads/2016/08/tatuagem-geom%C3%A9trica-de-animal.jpg' />
-                        <h3>Geometrico</h3>
-                    </div>
-                    <div className='game-list'>
-                        <img src='https://www.guiadasemana.com.br/contentFiles/image/2018/08/FEA/galeria/37265_w840h525_1533651154glitch.jpg' />
-                        <h3>Glitch</h3>
-                    </div>
-                    <div className='game-list'>
-                        <img src='https://www.guiadasemana.com.br/contentFiles/image/2018/08/FEA/galeria/37265_w840h525_1533592250tinta-vermelha.jpg' />
-                        <h3>Tinta vermelha</h3>
-                    </div>
-                    <div className='game-list'>
-                        <img src='https://www.guiadasemana.com.br/contentFiles/image/2018/08/FEA/galeria/37265_w840h525_1533591312oriental.jpg' />
-                        <h3>Oriental</h3>
-                    </div>
-                    <div className='game-list'>
-                        <img src='https://www.guiadasemana.com.br/contentFiles/image/2018/08/FEA/galeria/37265_w840h525_1533650371single-line.jpg' />
-                        <h3>Single line</h3>
-                    </div>
-                    </div>
-            </main>
+            </div>
+            <div className='bio'>
+                <h1>ETERNIZE SUAS IDEIAS</h1>
+                <h4>Gabi é formada em estética, e sua vontade de ajudar as pessoas a recuperar a autoestima foi o que a levou a fundar a Wolf Tattoo Studios.
+                    <br />Especialista em em tatuar cicatrizes, Gabi tem a missão de ressignificar marcas que são dolorosas as pessoas e transformá-las de forma positiva.
+                    <br />Esse tipo de de trabalho exige um grande dominío das técnicas, além de talento já que cicatrizes apresentam um outro tipo de pele, que recebe a tintura de forma diferente.
+                    <br />A Woolf Tatto Studios surgiu  para trazer um sorriso ao rosto das pessoas, independente do motivo para fazer uma arte.
+                    Seus principal estilo é o geométrico.</h4>
+            </div>
+            <div className='games'>
+                <div className='game-list'>
+                    <img src='https://www.guiadasemana.com.br/contentFiles/image/2018/08/FEA/galeria/37265_w840h525_1533591561geometrica.jpg' />
+                    <h3>Geometrico</h3>
+                </div>
+                <div className='game-list'>
+                    <img src='https://fotostatuagens.com/wp-content/uploads/2018/04/tattoo-geometrica.jpg' />
+                    <h3>Geometrico</h3>
+                </div>
+                <div className='game-list'>
+                    <img src='https://tudocommoda.com/wp-content/uploads/2016/08/tatuagem-geom%C3%A9trica-de-animal.jpg' />
+                    <h3>Geometrico</h3>
+                </div>
+                <div className='game-list'>
+                    <img src='https://www.guiadasemana.com.br/contentFiles/image/2018/08/FEA/galeria/37265_w840h525_1533651154glitch.jpg' />
+                    <h3>Glitch</h3>
+                </div>
+                <div className='game-list'>
+                    <img src='https://www.guiadasemana.com.br/contentFiles/image/2018/08/FEA/galeria/37265_w840h525_1533592250tinta-vermelha.jpg' />
+                    <h3>Tinta vermelha</h3>
+                </div>
+                <div className='game-list'>
+                    <img src='https://www.guiadasemana.com.br/contentFiles/image/2018/08/FEA/galeria/37265_w840h525_1533591312oriental.jpg' />
+                    <h3>Oriental</h3>
+                </div>
+                <div className='game-list'>
+                    <img src='https://www.guiadasemana.com.br/contentFiles/image/2018/08/FEA/galeria/37265_w840h525_1533650371single-line.jpg' />
+                    <h3>Single line</h3>
+                </div>
+            </div>
+            <br />
         </div>
-
     )
 }
 
