c6deefe5b4c21f0939048ecd6ab26ee362258078
Tamiresporto <112409145+Tamiresporto@users.noreply.github.com>
2022-11-21 10:54:43 -0300

    cards
    alteração dos cards, na cor da fonte, nome

--- src/Componentes/pages/Equipe/Equipe.jsx
+++ src/Componentes/pages/Equipe/Equipe.jsx
@@ -14,12 +14,13 @@ export const Equipe = () => {
         <div class="card1">
           <Card style={{ width: '20rem' }}>
             <Card.Img variant="top" src="/src/Componentes/pages/img/kezia.jfif" width="300" height="200" />
-            <Card.Body>
-              <Card.Title>Kesia</Card.Title>
+            <Card.Body><div class="texto">
+              <Card.Title><strong>Kesia</strong></Card.Title>
               <Card.Text>
                 <i>Gestora de Enganjamento</i><br></br>
+                
 
-              </Card.Text>
+              </Card.Text></div>
               <div class="buttom1"><Button variant="primary">Contato</Button></div>
             </Card.Body>
           </Card>
@@ -27,11 +28,11 @@ export const Equipe = () => {
         <div class="card2">
           <Card style={{ width: '20rem' }}>
             <Card.Img variant="top" src="/src/Componentes/pages/img/maicon.jfif" width="300" height="200" />
-            <Card.Body>
+            <Card.Body><div class="texto2">
               <Card.Title>Maicon</Card.Title>
               <Card.Text>
                 <i> Co-Facilitador</i><br></br>
-              </Card.Text>
+              </Card.Text></div>
               <Button variant="primary">Contato</Button>
             </Card.Body>
           </Card>
@@ -39,33 +40,33 @@ export const Equipe = () => {
         <div class="card3">
           <Card style={{ width: '20rem' }}>
             <Card.Img variant="top" src="/src/Componentes/pages/img/tamires.jfif" width="300" height="200" />
-            <Card.Body>
+            <Card.Body><div class="texto3">
               <Card.Title>Tamires</Card.Title>
               <Card.Text>
                 <i>Colaboradora l</i> <br></br>
-              </Card.Text>
+              </Card.Text></div>
               <Button variant="primary">Contato</Button>
             </Card.Body>
           </Card></div>
         <div class="card4">
           <Card style={{ width: '20rem' }}>
             <Card.Img variant="top" src="/src/Componentes/pages/img/bernardo.jfif" width="300" height="200" />
-            <Card.Body>
+            <Card.Body><div class="texto4">
               <Card.Title><i>Bernardo</i></Card.Title>
               <Card.Text>
                 <i>Gestora de Conhecimento</i><br></br>
-              </Card.Text>
+              </Card.Text></div>
               <Button variant="primary">Contato</Button>
             </Card.Body>
           </Card></div>
         <div class="card5">
           <Card style={{ width: '20rem' }}>
             <Card.Img variant="top" src="/src/Componentes/pages/img/grazi.jpg" width="300" height="200" />
-            <Card.Body>
+            <Card.Body><div class="texto5">
               <Card.Title><i>Grazi</i></Card.Title>
               <Card.Text>
                 Colaboradora ll<br></br>
-              </Card.Text>
+              </Card.Text></div>
               <Button variant="primary">Contato</Button>
             </Card.Body>
           </Card>
