67230f82cbf3af96c2c7b24567a8277f50b0c0e1
Grazielle Firmino <112557569+GraziFirmino@users.noreply.github.com>
2022-11-21 14:01:29 -0300

    Att perfil João
    atualização do perfil João

--- src/Componentes/layout/Tatuador1/Tatuador1.jsx
+++ src/Componentes/layout/Tatuador1/Tatuador1.jsx
@@ -22,7 +22,10 @@ function Tatuador1() {
                 </div>
                 <div className='bio'>
                     <h1>ETERNIZE SUAS IDEIAS</h1>
-                    <h4>João de 33 anos especialista há 15 anos em realismo, blackWork e old school. Ele iniciou sua carreira artística em 2001, quando cursou desenho artístico e aerografia. Esse foi o start para que em alguns anos mais tarde entrase para o universo das tatuagens </h4>
+                    <h4>João é carioca, formado em desing. Desde criança era apaixonado por tatuagem, principalmente pelas várias possibilidades de pigmentação e cores. 
+                        Pela falta de referências decidiu se especializar em trabalhos em pele negra trazendo um ambiente mais inclusivo, e elevando a autoestima das pessoas 
+                        que encontram na Wolf Tatto Studio um ambiente acolhedor para todos. 
+                        É especialista há 15 anos em realismo, blackWork e old school. Ele iniciou sua carreira artística em 2001, quando cursou desenho artístico e aerografia. Esse foi o start para que em alguns anos mais tarde entrase para o universo das tatuagens </h4>
                 </div>
             </div>
             
