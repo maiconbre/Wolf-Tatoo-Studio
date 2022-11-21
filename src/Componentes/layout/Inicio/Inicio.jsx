fbb81badf90c3498e0ec277ee9758caace1baa7a
devRaquel <112557569+GraziFirmino@users.noreply.github.com>
2022-11-21 12:00:43 -0300

    Update Inicio.jsx
    Adição texto da home

--- src/Componentes/layout/Inicio/Inicio.jsx
+++ src/Componentes/layout/Inicio/Inicio.jsx
@@ -11,7 +11,15 @@ const Inicio = props => {
                         <div className='hero-text'>
                             <h1 className='hero-title'>Eternize suas ideias</h1>
                             <p>
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, impedit quo. Officia nisi corrupti iste asperiores accusamus quisquam quod perspiciatis vitae? Esse eum molestiae ab quasi omnis quidem officia eaque.
+                            Wolf Tatoo Studio nasceu no Rio de Janeiro, com a proposta de trazer um ambiente mais inclusivo, diverso e acolhedor. São profissionais de áreas de distintas que se reuniram por um só propósito. O amor pela arte, pela expressão e pelo mundo das tatuagens.</p> 
+                            <p><i>
+                            “Nossos artistas são diferenciados, todos eles são de lugares diferentes, mundos diferentes, mas encontraram no nosso espaço um lugar que é para todos, em que o talento, a técnica e a liberdade de expressão são exaltados ao máximo atendendo todo tipo de gente.</i></p> 
+                            
+                            <p>
+
+                             Começou como um pequeno estúdio voltado para a área estética em tatuar regiões do corpo que antes traziam sofrimento para as pessoas, uma cicatriz pode se transformar numa rosa, num símbolo de força, isso é com o cliente! Mas eles saem felizes, satisfeitos e com a autoestima lá em cima. Logo, outros tatuadores se identificaram com nosso propósito e hoje temos orgulho de dizer que somos referência em técnicas diferenciadas, mas também fazemos nosso clientes felizes com o tradicional.
+
+                            Não importa o que você está procurando, certamente encontrará aqui!
                             </p>
                             <span className='w3-text-white social'>
                                 <p >Sigam nossas midias sociais</p>
