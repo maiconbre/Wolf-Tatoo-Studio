8e834a70c3176ba2601437dfa3e6e38b00a6428d
Kesia <112557978+kesia8@users.noreply.github.com>
2022-11-21 11:49:24 -0300

    modificando studio

--- src/Componentes/layout/Tatuador1/Tatuador1.jsx
+++ src/Componentes/layout/Tatuador1/Tatuador1.jsx
@@ -15,16 +15,17 @@ function Tatuador1() {
                         </div>
                         <div className='store-text-description'>
                             <h4>Campo Grande</h4>
-                            nv: 4 anos
+                            nv: 15 anos
                             <small>Pontilhismo Realismo Black Work Old School</small>
                         </div>
                     </div>
                 </div>
                 <div className='bio'>
                     <h1>ETERNIZE SUAS IDEIAS</h1>
-                    <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, impedit quo. Officia nisi corrupti iste asperiores accusamus quisquam quod perspiciatis vitae? Esse eum molestiae ab quasi omnis quidem officia eaque.</h4>
+                    <h4>João de 33 anos especialista há 15 anos em realismo, blackWork e old school. Ele iniciou sua carreira artística em 2001, quando cursou desenho artístico e aerografia. Esse foi o start para que em alguns anos mais tarde entrase para o universo das tatuagens </h4>
                 </div>
             </div>
+            
             <main>
                 <h2 className='title' id='artes'>Portifólio</h2>
                 <div className='games'>
@@ -32,18 +33,12 @@ function Tatuador1() {
                         <img src='https://www.guiadasemana.com.br/contentFiles/image/2018/08/FEA/galeria/37265_w840h525_1533590127old-school.jpg' alt='old school' />
                         <h3>Old School</h3>
                     </div>
-                    <div className='game-list'>
-                        <img src='https://www.guiadasemana.com.br/contentFiles/image/2018/08/FEA/galeria/37265_w840h525_1533591561geometrica.jpg' alt='Geométrico' />
-                        <h3>Geométrico</h3>
-                    </div>
+                   
                     <div className='game-list'>
                         <img src='https://www.guiadasemana.com.br/contentFiles/image/2018/08/FEA/galeria/37265_w840h525_1533592552realista.jpg' alt='Realista' />
                         <h3>Realista</h3>
                     </div>
-                    <div className='game-list'>
-                        <img src='https://www.guiadasemana.com.br/contentFiles/image/2018/08/FEA/galeria/37265_w840h525_1533590977aquarela.jpg' alt='Aquarela' />
-                        <h3>Aquarela</h3>
-                    </div>
+                    
                     <div className='game-list'>
                         <img src='https://www.guiadasemana.com.br/contentFiles/image/2018/08/FEA/galeria/37265_w840h525_1533654886blackwork-grande.jpg' alt='BlackWork' />
                         <h3>Black Work</h3>
@@ -52,15 +47,91 @@ function Tatuador1() {
                         <img src='https://www.guiadasemana.com.br/contentFiles/image/2018/08/FEA/galeria/37265_w840h525_1533589655pontilhismo.jpg' alt='Pontilhismo' />
                         <h3>Pontilhismo</h3>
                     </div>
+                    
+                   
+
+                </div>
+                <h2 className='title'>Bruno</h2>
+                <div className='game-store'>
+                    <div className='game-store-list'>
+                <div className='game-image'>
+                            <img src='https://img.freepik.com/fotos-gratis/retrato-de-homem-serio-e-barbudo-em-oculos-escuros-e-chapeu-de-cilindro_613910-1612.jpg?w=996&t=st=1669038734~exp=1669039334~hmac=0ee3165dd312ed4140992115896acaa52d15468f6f03fdc889841c0443d72fb9' />
+                        </div>
+                        <div className='store-text-description'>
+                            <h4>São Paulo</h4>
+                            nv: 10 anos
+                            <small>Maori, Aquarela, Sem contorno</small>
+                        </div>
+                        </div>
+                        </div>
+                        <div className='bio'>
+                    <h1>ETERNIZE SUAS IDEIAS</h1>
+                    <h4>Bruno sempre demonstrou habilidades com arte e apreço por tatuagens, fez sua primeira tattoo aos 18 anos, desde então seu interesse por este universo cresceu cada vez mais. Com ideias e princípios underground e alternativas, foi conquistando seu espaço com seu estilo próprio de tatuagens, sendo elas maori, aquarela, sem contorno.</h4>
+                </div>
+                <h2 className='title' id='artes'>Portifólio</h2>
+                <div className='games'>
                     <div className='game-list'>
-                        <img src='https://www.guiadasemana.com.br/contentFiles/image/2018/08/FEA/galeria/37265_w840h525_1533592955maori.jpg' alt='Maori' />
+                        <img src='https://www.guiadasemana.com.br/contentFiles/image/2018/08/FEA/galeria/37265_w840h525_1533590977aquarela.jpg' />
+                        <h3>aquarela</h3>
+                    </div>
+                    <div className='game-list'>
+                        <img src='https://www.guiadasemana.com.br/contentFiles/image/2018/08/FEA/galeria/37265_w840h525_1533653325costela7.jpg' />
+                        <h3>Sem contorno</h3>
+                    </div>
+                    <div className='game-list'>
+                        <img src='https://www.guiadasemana.com.br/contentFiles/image/2018/08/FEA/galeria/37265_w840h525_1533592955maori.jpg' />
                         <h3>Maori</h3>
                     </div>
-                    <div className='game-list'>
-                        <img src='https://www.guiadasemana.com.br/contentFiles/image/2018/08/FEA/galeria/37265_w840h525_1533653325costela7.jpg' alt='Sem Contorno' />
-                        <h3>Sem Contorno</h3>
                     </div>
+
+                <h2 className='title'>Gabi</h2>
+                <div className='game-store'>
+                    <div className='game-store-list'>
+                <div className='game-image'>
+                            <img src='https://img.freepik.com/fotos-gratis/jovem-mestre-de-tatuagem-focado-esta-fazendo-tatuagem-na-mao-do-cliente-no-salao-de-tatuagem-escuro_613910-5867.jpg?w=996&t=st=1669038700~exp=1669039300~hmac=1fe3678b6e2f80db5a477a5c8646ad660e724a7c6d4935d0a370d380e1711249' />
+                        </div>
+                        <div className='store-text-description'>
+                            <h4>Campo Grande</h4>
+                            nv: 5 anos
+                            <small>Geometrico, oriental, single line, glitch, tinta vermelha</small>
+                        </div>
+                        </div>
+                        </div>
+                        <div className='bio'>
+                    <h1>ETERNIZE SUAS IDEIAS</h1>
+                    <h4>27 anos, nascida em CG, sempre se interessou pelo mundo da arte, mas foi em 2017 que fez a melhor escolha de sua vida, comprou sua primeira máquina de tatuagem. Seus principal estilo é o geométrico.</h4>
                 </div>
+                <h2 className='title' id='artes'>Portifólio</h2>
+                <div className='games'>
+                    <div className='game-list'>
+                        <img src='https://www.guiadasemana.com.br/contentFiles/image/2018/08/FEA/galeria/37265_w840h525_1533591561geometrica.jpg' />
+                        <h3>Geometrico</h3>
+                    </div>
+                    <div className='game-list'>
+                        <img src='https://fotostatuagens.com/wp-content/uploads/2018/04/tattoo-geometrica.jpg' />
+                        <h3>Geometrico</h3>
+                    </div>
+                    <div className='game-list'>
+                        <img src='https://tudocommoda.com/wp-content/uploads/2016/08/tatuagem-geom%C3%A9trica-de-animal.jpg' />
+                        <h3>Geometrico</h3>
+                    </div>
+                    <div className='game-list'>
+                        <img src='https://www.guiadasemana.com.br/contentFiles/image/2018/08/FEA/galeria/37265_w840h525_1533651154glitch.jpg' />
+                        <h3>Glitch</h3>
+                    </div>
+                    <div className='game-list'>
+                        <img src='https://www.guiadasemana.com.br/contentFiles/image/2018/08/FEA/galeria/37265_w840h525_1533592250tinta-vermelha.jpg' />
+                        <h3>Tinta vermelha</h3>
+                    </div>
+                    <div className='game-list'>
+                        <img src='https://www.guiadasemana.com.br/contentFiles/image/2018/08/FEA/galeria/37265_w840h525_1533591312oriental.jpg' />
+                        <h3>Oriental</h3>
+                    </div>
+                    <div className='game-list'>
+                        <img src='https://www.guiadasemana.com.br/contentFiles/image/2018/08/FEA/galeria/37265_w840h525_1533650371single-line.jpg' />
+                        <h3>Single line</h3>
+                    </div>
+                    </div>
             </main>
         </div>
 
