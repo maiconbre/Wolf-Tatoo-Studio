b58a5450b011765d89ac82cfac38a1237bf22461
bedibe <beadelino@gmail.com>
2022-11-21 17:34:17 -0300

    ok

--- src/Componentes/pages/Forms/Forms.jsx
+++ src/Componentes/pages/Forms/Forms.jsx
@@ -47,6 +47,7 @@
           </label>
           <input type="text" placeholder='Estilo da tatuagem'/>
         </div>
+        <br></br>
         <div>
         <button type="submit" value="Enviar agendamento">Enviar agendamento</button>
         </div>
