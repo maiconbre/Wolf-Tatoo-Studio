56839676ea9d3fcef52c1a60251f4d0342f5e7e0
Tamires <112409145+Tamiresporto@users.noreply.github.com>
2022-11-22 01:09:58 -0300

    inserido o Post no forms

--- src/Componentes/pages/Forms/Forms.jsx
+++ src/Componentes/pages/Forms/Forms.jsx
@@ -1,63 +1,82 @@
- 
- import  '../Forms/Forms.css'
+
+import '../Forms/Forms.css'
 import App from './App';
- 
- 
 
+function Forms() {
+  const onSubmit = (event) => {
+    event.preventDefault()
+    console.log(event.target.elements.name.value)
+    async function postData(url = '', data = {}) {
+      // Default options are marked with *
+      const response = await fetch(url, {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc. 
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data) // body data type must match "Content-Type" header
+      });
+      return response.json(); // parses JSON response into native JavaScript objects
+    }
 
- function Forms() {
-
-  return(
+    postData('/tatuadores', {
+      name: event.target.elements.name.value, 
+      phone: event.target.elements.phone.value, 
+      tattoo_style: event.target.elements.tattoo_style.value,
+      tatuador_id: event.target.elements.tatuador_id.value, 
+      date: event.target.elements.date.value
+    })
+      .then((data) => {
+        console.log(data); // JSON data parsed by `data.json()` call
+      });
+  }
+  return (
     <div className='forms_container'>
-      <h1>Faça seu agendamento</h1>
-      <hr/>
-      <App />
-      <br></br>
-      <hr></hr>
-      <form>
+
+
+      <form onSubmit={onSubmit}>
         <div>
           <label>
             Nome
           </label>
-          <input type="text" text="Nome" name="name" placeholder="Nome" /> 
+          <input type="text" text="Nome" name="name" placeholder="Nome" />
         </div>
         <div>
           <label>
             Telefone de contato
           </label>
-        <input type="tel" placeholder='Insira seu numero' />
+          <input type="tel" name="phone" placeholder='Insira seu numero' />
         </div>
         <div>
           <label>
-          Data da Sessão
+            Data da Sessão
           </label>
-        <input type="date" placeholder='Data' />
+          <input type="date" name="date" placeholder='Data' />
         </div>
         <div>
           <label>
             Tatuador
-            </label>
-        <select name="tatuador_id">
-          <option>Selecione o tatuador</option>
-          <option id='1'>Brenda Palácios </option>
-          <option id='2'>Benicio Braga</option>
-          <option id='3'>Fernando Torres</option>
-          <option id='4'>Marilia Fernandes</option>
-        </select>
+          </label>
+          <select name="tatuador_id">
+            <option>Selecione o tatuador</option>
+            <option id='1'>Brenda Palácios </option>
+            <option id='2'>Benicio Braga</option>
+            <option id='3'>Fernando Torres</option>
+            <option id='4'>Marilia Fernandes</option>
+          </select>
         </div>
         <div>
           <label>
             Estilo de tatuagem
           </label>
-          <input type="text" placeholder='Estilo da tatuagem'/>
+          <input type="text" name="tattoo_style" placeholder='Estilo da tatuagem' />
         </div>
-        <br></br>
         <div>
-        <button type="submit" value="Enviar agendamento">Enviar agendamento</button>
+          <button type="submit" event="submit" value="Enviar agendamento">Enviar agendamento</button><App />
         </div>
+
       </form>
     </div>
   )
- }
+}
 
- export default Forms;
+export default Forms;
